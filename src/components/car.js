4df6357caed68dce112bd3b985a028c914147b32
liuh <liuh@FleetPlace2021.local>
2021-10-08 18:00:53 -0400

    add styles for car component

--- src/components/car.js
+++ src/components/car.js
@@ -1,14 +1,17 @@
 import RedirectButton from './redirectButton'
+import * as styles from './car.module.css'
 
-const car = (carsLink) => {
+const car = (carsLink, carColor) => {
     return (
-        <div> 
+        <div className={styles.carContainer}> 
             <h3>ARE YOU SURE? </h3>
-            <p>You want to leave the site to see the (black) car?</p>
-            <img src={"images/blackCar.png"} style={{height: "150px", width: "150px"}} />
-            <div>
-                <RedirectButton  carsLink={carsLink} buttonColor="white" buttonText="YES" />
-                <RedirectButton  buttonColor="white" buttonText="No" />
+            <p>You want to leave the site to see the (carColor vairable) car?</p>
+            <div className={styles.carButtons}>
+                <img src={"images/blackCar.png"} style={{height: "150px", width: "150px"}} />
+                <div className={styles.buttons}>
+                    <RedirectButton  carsLink={carsLink} buttonColor="white" buttonText="YES" />
+                    <RedirectButton  buttonColor="white" buttonText="No" />
+                </div>
             </div>
             <p>*Please remember to wear your seatbelt</p>
         </div>
