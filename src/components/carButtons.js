9c3a50bc0b981076f44f26b9164e597be33118e0
liuh <liuh@FleetPlace2021.local>
2021-10-08 12:40:58 -0400

    remove react default logo and defaul headers. Add basic home page header and buttons

---
+++ src/components/carButtons.js
@@ -0,0 +1,12 @@
+
+const carButtons = () => {
+    return (
+        <div>
+            <button>BLACK</button>
+            <button>RED</button>
+            <button>BLUE</button>
+        </div>
+    )
+}
+
+export default carButtons;
